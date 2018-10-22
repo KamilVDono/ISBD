9ed4572de3ca5b18b0e7f79ad83dbf2dcc31c2bf
KamilVDono <kamilvdono@gmail.com>
2018-10-22 13:37:26 +0200

    Database added, folder structures added, simple model added

---
+++ Model/OsobaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISBD.Model
+{
+    class OsobaModel
+    {
+        public long IdO{ get; set; }
+        public string Login { get; set; }
+        public string Haslo { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public DateTime DataU { get; set; }
+    }
+}
