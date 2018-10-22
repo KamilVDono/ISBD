0d462f17984845a14c1c043c6a61585b5924900f
KamilVDono <kamilvdono@gmail.com>
2018-10-22 22:12:52 +0200

    Added Singleton generic class

---
+++ Utils/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISBD.Utils
+{
+	class Singleton<T> where T : class , new()
+	{
+		private static T _instance = null;
+		static readonly object _padlock = new object();
+		public static T Instance
+		{
+			get
+			{
+				lock (_padlock)
+				{
+					return _instance ?? (_instance = new T());
+				}
+			}
+			private set { _instance = value; }
+		}
+	}
+}
