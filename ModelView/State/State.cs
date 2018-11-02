353dac0b3940b1b77ef9c99e478817a8f599a940
KamilVDono <kamilvdono@gmail.com>
2018-11-03 00:57:04 +0100

    Fixed bugs

---
+++ ModelView/State/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISBD.ModelView.State
+{
+	public class StatePushParameters
+	{
+
+	}
+
+	public abstract class State
+	{
+		public abstract void Push(StatePushParameters pushParameters);
+		public abstract void StartState();
+		public abstract void PauseState();
+		public abstract void ResumeState();
+		public abstract void StopState();
+	}
+}
--- ModelView/StateMachine.cs
+++ ModelView/StateMachine.cs
@@ -1,24 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ISBD.Model.Tests;
+using ISBD.ModelView.State;
 using ISBD.Utils;
 
 namespace ISBD.ModelView
 {
 	public class StateMachine: Singleton<StateMachine>
 	{
-		public List<Action> StartupActions = new List<Action>()
+		public static readonly Type START_TYPE = typeof(StartupState);
+
+		private List<Action> StartupActions = new List<Action>()
 		{
 			() => {new QueryTest();},
+		};
+		private List<Type> StateTypes = new List<Type>()
+		{
 
 		};
+
+		private Dictionary<Type, State.State> StateInstances = new Dictionary<Type, State.State>();
+		private Stack<State.State> StatesStack = new Stack<State.State>();
 
 		public StateMachine()
 		{
 			StartupActions.ForEach(action => action());
+			InitDictionary();
+			SetStartupState();
+		}
+
+		public void PushState<T>(StatePushParameters parameters) where T : State.State
+		{
+			GetStateInstance<T>(out var stateToPush);
+			if (stateToPush == null)
+			{
+				return;
+			}
+			stateToPush.Push(parameters);
+
+			if (PopUntil<T>())
+			{
+				stateToPush.ResumeState();
+			}
+			else
+			{
+				StatesStack.Peek().PauseState();
+				StatesStack.Push(stateToPush);
+				stateToPush.StartState();
+			}
+		}
+
+		public bool PopUntil<T>() where T : State.State
+		{
+			GetStateInstance<T>(out var state);
+			if (state == null)
+			{
+				return false;
+			}
+
+			if (StatesStack.Contains(state))
+			{
+				while (StatesStack.Peek() != state)
+				{
+					StatesStack.Pop().StopState();
+				}
+				return true;
+			}
+
+			return false;
+		}
+
+		public void PopState()
+		{
+			if (StatesStack.Count > 1)
+			{
+				StatesStack.Pop().StopState();
+			}
+			System.Console.Error.WriteLine("There is only one state in stack, can not pop this");
+		}
+
+		public State.State CurrentState()
+		{
+			return StatesStack.Peek();
+		}
+
+		public bool IsStateInStack<T>() where T : State.State
+		{
+			GetStateInstance<T>(out var state);
+			return state != null && StatesStack.Contains(state);
+		}
+
+		public T GetStateInstance<T>() where T : State.State
+		{
+			if (StateInstances.TryGetValue(typeof(T), out var state) == false)
+			{
+				System.Console.Error.WriteLine($"There is no {typeof(T).FullName} state in states dictionary, please fix it!");
+				return null;
+			}
+
+			return (T)state;
+		}
+
+		public void GetStateInstance<T>(out T state) where T : State.State
+		{
+			state = GetStateInstance<T>();
+		}
+
+		private void InitDictionary()
+		{
+			StateTypes.Clear();
+			StateTypes = (new HashSet<Type>(StateTypes)).ToList();
+			StateTypes.ForEach(stateType =>
+			{
+				if (stateType.IsSubclassOf(typeof(State.State)))
+				{
+					var state = (State.State)Activator.CreateInstance(stateType);
+					StateInstances.Add(stateType, state);
+				}
+			} );
+		}
+
+		private void SetStartupState()
+		{
+			if (StateInstances.TryGetValue(START_TYPE, out var state) == false)
+			{
+				System.Console.Error.WriteLine($"There is no {START_TYPE.FullName} state in states dictionary but is market as START_TYPE, please fix it!");
+				throw new ArgumentNullException();
+			}
+			state.Push(null);
+			StatesStack.Push(state);
+			state.StartState();
 		}
 	}
 }
