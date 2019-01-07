2cb47cc630ab455f69a09e3abb6acf31b3469ca4
Eryk Sapijaszko <sapijaszkoeryk@gmail.com>
2019-01-07 10:57:35 +0100

    register done in 99%

---
+++ ModelView/State/Connectors/IRegisterUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+
+namespace ISBD.ModelView.State.Connectors
+{
+    public interface IRegisterUI
+    {
+        string Login { get; }
+
+        void _Login(bool imageType, string message);
+
+        string Password { get; }
+        void _Password(bool imageType, string message);
+
+        string RepeatPassword { get; }
+        //UName == User name
+        string UName { get; }
+        void _UName(bool imageType, string message);
+        string Surname { get; }
+
+        void _Surname(bool imageType, string message);
+
+        Button Next { get; }
+        Button Back { get; }
+
+    }
+}
